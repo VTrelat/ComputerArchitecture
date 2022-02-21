2484253ad21cd922adcbda0a020d0a1609289b59
Vincent Trélat <vincent.trelat@free.fr>
2022-02-21 09:46:19 +0100

    Updates

---
+++ ex4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Sizeof int: %lu octets\n", sizeof(int));
+    printf("Sizeof short: %lu octets\n", sizeof(short));
+    printf("Sizeof char: %lu octets\n", sizeof(char));
+    return 0;
+}
