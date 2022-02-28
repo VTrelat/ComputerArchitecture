5260185b35f7296231ad83812ecbe568fa6823b4
Vincent Trélat <vincent.trelat@free.fr>
2022-02-28 11:40:36 +0100

    Updates

--- c/ch2/ex7.c
+++ c/ch2/ex7.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 #include <x86intrin.h>
 
-void test(int a)
-{
-    int s = 0;
-    for (int i = 0; i < a; i++)
-    {
-        s += i * i;
-    }
-}
-
-void acces(int *tab, int n)
+void espacement(int *tab, int pas)
 {
     int tmp;
-    for (int i = 0; i < n; i++)
+    register unsigned int i = 0;
+    register unsigned long int bound = pas * 10000000;
+    while (i < bound)
     {
-        tmp = tab[i];
+        tmp = tab[i % 1000000000];
+        i += pas;
     }
 }
 
 void print_timing(int *arg1, int arg2, void (*func)(int *, int), int nb_boot, int nb_call)
 {
-    // votre code peut appeler la fonction à mesurer
-    // par ma_fonction(arg)
-    // il doit afficher (printf) le résultat de la mesure
-
     // amorçage
     for (int i = 0; i < nb_boot; i++)
     {
@@ -35,22 +25,23 @@ void print_timing(int *arg1, int arg2, void (*func)(int *, int), int nb_boot, in
     unsigned long int tic, toc;
     unsigned int ui;
     tic = __rdtscp(&ui);
+
     for (int i = 0; i < nb_call; i++)
     {
         func(arg1, arg2);
     }
-    toc = __rdtscp(&ui);
 
+    toc = __rdtscp(&ui);
     printf("average time : %lu\n", (toc - tic) / nb_call);
 }
 
 int main()
 {
     int *tab = malloc(1000000000 * sizeof(int));
-    for (unsigned long int i = 1000; i < 1000000001; i *= 10)
+    for (unsigned long int i = 0; i < 1001; i += 50)
     {
         printf("[i=%lu]\n", i);
-        print_timing(tab, i, acces, 5, 10);
+        print_timing(tab, i, espacement, 5, 10);
     }
     return 0;
 }
