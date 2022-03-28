773a00bfbd8075296e70ce7ec5cf23979f4c3318
Vincent Trélat <vincent.trelat@free.fr>
2022-03-28 10:22:03 +0200

    Updates

--- c/ch5/ex2.c
+++ c/ch5/ex2.c
@@ -1,39 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <x86intrin.h>
 
 int *make_random_table(int n)
 {
     int *table;
-    int i;
+    register int i;
 
     table = malloc(n * sizeof(int));
     if (table == NULL)
         return NULL;
 
     for (i = 0; i < n; i++)
-        table[i] = rand();
+        table[i] = rand() % 100;
 
     return table;
+}
+
+int count_less_than(int *tab, int n, int threshold)
+{
+    register int i;
+    register int count = 0;
+
+    for (i = 0; i < n; i++)
+        if (tab[i] < threshold)
+            count++;
+
+    return count;
+}
+
+void print_timing(int *tab, int n, int threshold, int (*func)(int *, int, int), int nb_boot, int nb_call)
+{
+    int s = 0;
+    // boot
+    for (int i = 0; i < nb_boot; i++)
+    {
+        s += func(tab, n, threshold);
+    }
+
+    // measure
+    unsigned long int tic, toc;
+    unsigned int ui;
+    tic = __rdtscp(&ui);
+    for (int i = 0; i < nb_call; i++)
+    {
+        s += func(tab, n, threshold);
+    }
+    toc = __rdtscp(&ui);
+
+    printf("%lu\n", (toc - tic) / nb_call);
 }
 
 int main(void)
 {
     int *table;
-    int i;
 
     srand(time(NULL));
-    table = make_random_table(10);
-    if (table == NULL)
+    table = make_random_table(10000000);
+    printf("i t\n");
+    for (register int i = 0; i < 100; i++)
     {
-        printf("Memory allocation failed.\n");
-        return 1;
+        printf("%d ", i);
+        print_timing(table, 10000000, i, count_less_than, 10, 100);
     }
-
-    for (i = 0; i < 10; i++)
-        printf("%d ", table[i]);
-    printf("\n");
-
     free(table);
+
     return 0;
 }
